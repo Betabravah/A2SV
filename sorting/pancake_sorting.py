3fd553d100d36eabb556cf5cb338a3a0e0769951
Betabravah <betabravah@gmail.com>
2022-10-15 16:04:28 +0300

    solved

--- sorting/pancake_sorting.py
+++ sorting/pancake_sorting.py
@@ -4,11 +4,11 @@ class Solution:
         placed = len(arr)
         while arr != sorted(arr):
             
-            if max(arr[:placed]) == arr[0]:
+            if max(arr[:placed]) == arr[0]: # flip it and make it last
                 flips.append(placed)
                 arr[:placed] = arr[placed-1::-1]
                 
-            elif max(arr[:placed]) != arr[placed-1]:
+            elif max(arr[:placed]) != arr[placed-1]: # bring it to index 0 and flip it to make it last
                 index = arr.index(max(arr[:placed])) + 1
                 flips.append(index)
                 arr[:index] = arr[index-1::-1]
