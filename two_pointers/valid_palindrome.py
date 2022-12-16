66ce3032739d2d9800bed5596a99607692979403
your github username <your email>
2022-12-16 19:47:01 +0300

    solved

---
+++ two_pointers/valid_palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        i = 0
+        j = len(s) - 1
+        while i <= j:
+            if s[i].isalnum():
+                if s[j].isalnum():
+                    if s[i] != s[j]:
+                        return False
+                    i += 1
+                    j -= 1
+
+                else:
+                    j -= 1
+            else:
+                i += 1
+
+
+        return True
