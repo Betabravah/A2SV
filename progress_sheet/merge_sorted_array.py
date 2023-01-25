d810514da1563ee72c9b64d63745e755e462ac36
Betabravah <101593044+Betabravah@users.noreply.github.com>
2023-01-25 17:11:35 +0300

    solved

---
+++ progress_sheet/merge_sorted_array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        position = n + m - 1
+        left, right = m-1, n-1
+
+        while position >= 0:
+            if left >= 0 and right >= 0:
+                if nums1[left] > nums2[right]:
+                    nums1[position], nums1[left] = nums1[left], nums1[position]
+                    left -= 1
+                    position -= 1
+
+                else:
+                    nums1[position], nums2[right] = nums2[right], nums1[position]
+                    right -= 1
+                    position -= 1
+            else:
+                if left >= 0:
+                    nums1[position], nums1[left] = nums1[left], nums1[position]
+                    left -= 1
+                    position -= 1
+                elif right >= 0:
+                    nums1[position], nums2[right] = nums2[right], nums1[position]
+                    right -= 1
+                    position -= 1
+
