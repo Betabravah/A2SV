124e9057aa4f1bb83c753f0757a5b96e6fd9b512
Betabravah <101593044+Betabravah@users.noreply.github.com>
2023-02-04 18:32:23 +0300

    solved

---
+++ progress_sheet/largest_number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str, nums))
+
+        def compare(num1, num2):
+            if num1 + num2 > num2 + num1:
+                return -1
+            else:
+                return 1
+
+        nums = sorted(nums, key=cmp_to_key(compare))
+
+        return str(int(''.join(nums)))
