eded1194dd138aa33e012813b65c492df5f16ebd
Betabravah <101593044+Betabravah@users.noreply.github.com>
2023-02-09 00:32:31 +0300

    solved

---
+++ progress_sheet/largest_merge_of_two_strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestMerge(self, word1: str, word2: str) -> str:
+        merged  =""
+        while word1 and word2:
+            if word1 <= word2:
+                merged += word2[0]
+                word2 = word2[1:]
+            else:
+                merged += word1[0]
+                word1= word1[1:]
+        
+        if word1:
+            merged += word1
+        else:
+            merged += word2
+        return merged
