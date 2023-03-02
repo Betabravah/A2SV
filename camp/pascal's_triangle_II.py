1637fe06a43f9f11b9dfd4178c41ff5d8807d5c7
Betabravah <101593044+Betabravah@users.noreply.github.com>
2023-03-02 15:28:10 +0300

    solved

---
+++ camp/pascal's_triangle_II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        else:
+            prevRow = self.getRow(rowIndex-1)
+            curRow = [1]
+            for i in range(1, rowIndex):
+                curRow.append(prevRow[i-1] + prevRow[i])
+
+            curRow.append(1)
+        return curRow
