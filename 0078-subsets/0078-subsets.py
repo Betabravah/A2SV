d5175795fffdfe8e069aec7082e732c42e651f40
Betabravah <101593044+Betabravah@users.noreply.github.com>
2023-03-10 16:26:41 +0300

    Time: 31 ms (88.27%), Space: 14.1 MB (77.75%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,10 +2,11 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         subsets = [[]]
         n = len(nums)
+        
         def backtrack(idx, path):
             if idx == n:
                 return
-            # subsets.append(path.copy())
+
             path.append(nums[idx])
             for i in range(idx+1, n):
                 backtrack(i, path)
