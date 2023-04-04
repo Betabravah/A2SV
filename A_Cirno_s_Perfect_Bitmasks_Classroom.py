13d9b2f51e5faf958831db671df82335a0e7ef24
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-04-04 19:32:06 +0300

    Create A_Cirno_s_Perfect_Bitmasks_Classroom.py

---
+++ A_Cirno_s_Perfect_Bitmasks_Classroom.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for _ in range(t):
+    x = int(input())
+    y = 0
+    
+    if x == 1:
+        y = 3
+
+    elif x & (x - 1) == 0:
+        y = x + 1
+
+    else:
+        y = (x & (-x))
+     
+    print(y)
