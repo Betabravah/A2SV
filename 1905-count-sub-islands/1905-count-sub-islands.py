af4565f398b3b37696e8f889d1c2e3e45da95bc8
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-04-16 15:30:17 +0300

    Time: 3396 ms (28.09%), Space: 107.5 MB (31.53%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -0,0 +1,40 @@
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        rows, cols = len(grid1), len(grid1[0])
+        
+        
+        def isbound(row, col):
+            return 0 <= row < rows and 0 <= col < cols
+        
+        
+        visited = set()
+        def dfs(row, col):
+            
+            visited.add((row, col))
+            
+            
+            for row_change, col_change in directions:
+                new_row = row + row_change
+                new_col = col + col_change
+                
+                if isbound(new_row, new_col) and (new_row, new_col) not in visited and grid2[new_row][new_col] == 1:
+                    dfs(new_row, new_col)
+                    
+                    
+        for i in range(rows):
+            for j in range(cols):
+                if (i, j) not in visited and grid1[i][j] == 0 and grid2[i][j] == 1:
+                    dfs(i, j)
+                    
+        islands = 0
+        for i in range(rows):
+            for j in range(cols):
+                if (i, j) not in visited and grid2[i][j] == 1:
+                    islands += 1
+                    dfs(i, j)
+                    
+        return islands
+                
+                
+                
