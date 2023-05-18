a74036b7796fcfacfd266bfed62a72aa4206ff85
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-05-18 19:05:32 +0300

    Time: 1553 ms (94.96%), Space: 61.1 MB (81.40%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,27 +1,38 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        root = [i for i in range(n+1)]
+        rank = [1] * (n + 1)
+        min_score = [inf for i in range(n+1)]
         
-        for i, j, cost in roads:
-            graph[i].append([j, cost])
-            graph[j].append([i, cost])
-            
-            
-        queue = deque([[1, inf]])
-        visited = set()
-        minn = inf
         
-        while queue:
-
-            cur, cost = queue.popleft()
+        def find(node):
+            if root[node] != node:            
+                root[node] = find(root[node])
+            return root[node]
+            
+        
+        for i, j, distance in roads:
+            
+            repi, repj = find(i), find(j)
+            
+            ranki, rankj = rank[repi], rank[repj]
+            minn = min(min_score[root[repi]], min_score[root[repj]], distance)
             
             
-            for child, child_cost in graph[cur]:
-                minn = min(minn, child_cost)
+            if rank[repi] == rank[repj]:
+                rank[repi] += 1
                 
-                if child not in visited:
-                    queue.append([child, child_cost])
-                    visited.add(child)
-            
-        return minn
+            if rank[repi] < rank[repj]:
+                root[repi] = repj
+                min_score[repj] = minn
+            else:
+                root[repj] = repi
+                min_score[repi] = minn
+                
+                
+        return min_score[find(1)]
+                
+                
+                
+                
             
