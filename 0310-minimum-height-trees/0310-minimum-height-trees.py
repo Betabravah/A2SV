f7e1477e59b2b6bbf5f8f1c02b186f632bcb8e7d
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-05-24 07:38:41 +0300

    Time: 527 ms (61.05%), Space: 26.3 MB (52.53%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -19,23 +19,25 @@ class Solution:
         height = 0
         queue = deque([i for i in range(n) if count[i] == 1])
 
-        while n > 2:
+
+        while queue:
+            
             length = len(queue)
-            n -= length
+            ans = []
             
             for _ in range(length):
                 cur = queue.popleft()
-
+                ans.append(cur)
+                
                 for child in graph[cur]:
                     count[child] -= 1
 
                     if count[child] == 1:
                         queue.append(child)
 
-                height += 1
+                
 
-
-        return list(queue)
+        return ans
 
         
                         
