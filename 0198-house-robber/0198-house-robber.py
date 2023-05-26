1ecdf3d1dbc0f2ed9b9930c8e177093c50b8411c
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-05-26 19:17:35 +0300

    Time: 42 ms (42.13%), Space: 16.6 MB (13.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,17 +3,17 @@ class Solution:
         memo = defaultdict(int)
         
         def rob(n):
-            if n < 0:
-                return 0
+            if n == 0:
+                return nums[0]
             
-            if n == 1 or n == 0:
-                return nums[n]
+            if n == 1 :
+                return max(nums[1], nums[0])
             
             if n not in memo:
-                memo[n] = max(rob(n - 2), rob(n - 3)) + nums[n]
+                memo[n] = max(rob(n-1), rob(n-2) + nums[n])
                 
             return memo[n]
         
         
-        return max(rob(len(nums) - 1), rob(len(nums) - 2))
+        return rob(len(nums) - 1)
                 
