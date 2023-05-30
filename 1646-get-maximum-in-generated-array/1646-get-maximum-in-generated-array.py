e07af37f92a46ad1636fb4475dca21d38f4feab6
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-05-30 17:43:01 +0300

    Time: 46 ms (30.50%), Space: 16.5 MB (11.56%) - LeetHub

---
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        memo = defaultdict(int)
+        memo[0] = 0
+        memo[1] = 1
+        
+        if n == 0:
+            return 0
+        
+        def dp(n):
+            nonlocal memo
+            if n <= 1:
+                return n
+            
+            if n not in memo:
+                if n % 2 == 1:
+                    memo[n] = dp(n // 2) + dp(ceil(n / 2))
+                    
+                else:
+                    memo[n] = dp(n // 2)
+                    
+            return memo[n]
+
+        for i in range(n+1):
+            dp(i)
+            
+        ans = -inf
+        for i in memo:
+            ans = max(ans, memo[i])
+        return ans
