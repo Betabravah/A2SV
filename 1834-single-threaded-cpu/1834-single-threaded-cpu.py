99787684a2701538b83e3bc66cb447fc958eabff
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-05-30 19:49:11 +0300

    Time: 2155 ms (24.99%), Space: 65.5 MB (17.80%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,10 +1,8 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         n = len(tasks)
-        
         for i in range(n):
             tasks[i].append(i)
-            
         tasks.sort(key=lambda i: i[0])
         
         heap = []
@@ -14,12 +12,13 @@ class Solution:
         
         
         while i < n:
-            
-            while i < n and tasks[i][0] <= cur_time:
-
-                task = [tasks[i][1], tasks[i][-1], tasks[i][0]]
-                heappush(heap, task)
-                i += 1
+            if not heap:
+                new_time = tasks[i][0]
+                while i < n and tasks[i][0] <= cur_time:
+                    
+                    task = [tasks[i][1], tasks[i][-1], tasks[i][0]]
+                    heappush(heap, task)
+                    i += 1
                 
                 
             if heap:
@@ -27,19 +26,21 @@ class Solution:
                 ans.append(idx)
                 cur_time += time
                 
-                
+                while i < n:
+                    
+                    if tasks[i][0] <= cur_time:
+                        task = [tasks[i][1], tasks[i][-1], tasks[i][0]]
+                        heappush(heap, task)
+                        i += 1
+                    else:
+                        break
             else:
                 cur_time = tasks[i][0]
-                
-
-                        
+        
         
         while heap:
             time, idx, _ = heappop(heap)
             ans.append(idx)
                     
         return ans
-                    
-                    
-                    
                     
