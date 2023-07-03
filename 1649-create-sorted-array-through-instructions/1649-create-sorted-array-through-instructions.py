773fa4e1cbca5828d272e6988e16510b4a715030
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-07-03 08:42:51 +0300

    Time: 4982 ms (68.25%), Space: 31 MB (73.02%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -4,12 +4,13 @@ class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
         nums = SortedList()
         count = 0
-
-        for i, num in enumerate(instructions):
-
-            greater = nums.bisect_right(num)
-            less = nums.bisect_left(num)
+        
+        for idx, num in enumerate(instructions):
+            idx1, idx2 = nums.bisect_left(num), nums.bisect_right(num)
+            
+                
+            count += min(idx1, idx - idx2)
             nums.add(num)
-            count += min(less, i - greater)
-
+            
         return count % (10**9 + 7)
+            
