0da0c7b3da0f98ece2bd78286019d527a67cfdd5
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-08-10 19:17:54 +0300

    Time: 40 ms (75.45%), Space: 16.2 MB (75.23%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,5 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        
+        return (len(set(pattern)) == len(set(words)) == len(set(zip_longest(pattern, words))))
