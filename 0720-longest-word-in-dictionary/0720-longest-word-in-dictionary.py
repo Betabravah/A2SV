c2acfe77f09baa1ac9db3ce4153fda3df8eb0c71
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-09-26 14:43:38 +0300

    Time: 143 ms (31.65%), Space: 16.9 MB (66.23%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -0,0 +1,60 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+    
+    
+class Trie:
+    
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word):
+            
+        cur = self.root
+        
+        for idx, i in enumerate(word):
+            
+            if idx < len(word) - 1:
+                if i not in cur.children:
+                    return 0
+                
+            else:
+                cur.children[i] = TrieNode()
+                
+            cur = cur.children[i]
+            
+        cur.is_end = True
+        
+        return len(word)
+
+
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        
+        trie = Trie()
+        max_len = 0
+        words.sort()
+        ans = []
+        
+        
+        for word in words:
+            length = trie.insert(word)
+            
+            if length > max_len:
+                ans = [word]
+                max_len = length
+                
+            elif length > 0 and length == max_len:
+                ans.append(word)
+                
+            
+        ans.sort()
+        return ans[0] if ans else ''
+            
+            
+                    
+        
+        
+        
+        
