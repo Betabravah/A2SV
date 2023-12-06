520ef343277ac4e607658ff2aa4401aaa8c3ac66
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-12-06 14:14:04 +0300

    Time: 51 ms (72.94%), Space: 16.3 MB (67.06%) - LeetHub

--- 0789-escape-the-ghosts/0789-escape-the-ghosts.py
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -1,11 +1,10 @@
 class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-        dist = abs(target[0]) + abs(target[1])
-        
-        for i in ghosts:
-            cur_dist = abs(i[0]-target[0]) + abs(i[1]-target[1])
-            
-            if cur_dist <= dist:
+        player = (abs(target[0]) + abs(target[1]))
+
+        for ghost in ghosts:
+            val = (abs(target[0] - ghost[0]) + abs(target[1] - ghost[1]))
+            if val <= player:
                 return False
-            
+        
         return True
