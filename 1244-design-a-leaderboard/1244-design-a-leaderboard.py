217db6b7ce109a4071ea3d4f7fcfbf79a2ad929f
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-12-07 15:00:37 +0300

    Time: 64 ms (71.80%), Space: 17 MB (5.84%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -1,25 +1,49 @@
+from sortedcontainers import SortedDict
 class Leaderboard:
 
     def __init__(self):
         self.score = defaultdict(int)
+        self.sorted_score = SortedDict()
         
 
     def addScore(self, playerId: int, score: int) -> None:
+        prev_score = self.score[playerId]
+        
+        if prev_score:
+            self.sorted_score[-prev_score] -= 1
+            
+            if self.sorted_score[-prev_score] == 0:
+                self.sorted_score.pop(-prev_score)
+                
+            new_score = prev_score + score
+            self.sorted_score[-new_score] = self.sorted_score.get(-new_score, 0) + 1
+            
+        else:
+            self.sorted_score[-score] = self.sorted_score.get(-score, 0) + 1
+        
+        
         self.score[playerId] += score
 
     def top(self, K: int) -> int:
-        items = sorted(self.score.items(), key=lambda x: x[1])
-        items = items[-K:]
-        
-        
-        
         ans = 0
-        for i in items:
-            ans += i[1]
+        for score, players in self.sorted_score.items():
+            possible = min(K, players)
             
+            ans += (-score * possible)
+            
+            K -= possible
+            if K == 0:
+                break
+                
         return ans
 
     def reset(self, playerId: int) -> None:
+        score = self.score[playerId]
+        
+        self.sorted_score[-score] -= 1
+        if self.sorted_score[-score] == 0:
+            self.sorted_score.pop(-score)
+            
         self.score[playerId] = 0
 
 
